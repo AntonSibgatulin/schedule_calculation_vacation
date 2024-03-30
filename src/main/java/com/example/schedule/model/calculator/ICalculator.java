3439ae446e406e3304dacfe4fde2e3c58da9827f
anton <89879639755a@gmail.com>
2024-03-30 13:43:48 +0300

    Сделал интерфейс

--- src/main/java/com/example/schedule/model/calculator/ICalculator.java
+++ src/main/java/com/example/schedule/model/calculator/ICalculator.java
@@ -1,2 +1,8 @@
-package com.example.schedule.model.calculator;public interface ICalculator {
+package com.example.schedule.model.calculator;
+
+import com.example.schedule.controller.calculation.dto.Vacation;
+
+public interface ICalculator {
+    Double calculateVacationAllowance(Vacation vacation);
+    Integer numberOfDays(Vacation vacation);
 }
--- src/main/java/com/example/schedule/model/holiday/IHoliday.java
+++ src/main/java/com/example/schedule/model/holiday/IHoliday.java
@@ -1,2 +1,14 @@
-package com.example.schedule.model.holiday;public interface IHoliday {
+package com.example.schedule.model.holiday;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface IHoliday {
+    List<LocalDate> initializeHolidays(int year);
+
+    boolean isHoliday(LocalDate date);
+
+    boolean isWorkingDay(LocalDate date);
+
+    List<LocalDate> getHolidays();
 }
