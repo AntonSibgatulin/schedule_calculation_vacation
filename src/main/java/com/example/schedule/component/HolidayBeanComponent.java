3834134d3157be2fd15d9c1b18de7a7027e9341e
anton <89879639755a@gmail.com>
2024-03-30 13:43:13 +0300

    реализовал IHoliday

--- src/main/java/com/example/schedule/component/HolidayBeanComponent.java
+++ src/main/java/com/example/schedule/component/HolidayBeanComponent.java
@@ -1,2 +1,14 @@
-package com.example.schedule.component;public class HolidayBeanComponent {
+package com.example.schedule.component;
+
+import com.example.schedule.model.holiday.HolidayImpl;
+import com.example.schedule.model.holiday.IHoliday;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HolidayBeanComponent {
+    @Bean
+    public IHoliday iHoliday() {
+        return new HolidayImpl();
+    }
 }
