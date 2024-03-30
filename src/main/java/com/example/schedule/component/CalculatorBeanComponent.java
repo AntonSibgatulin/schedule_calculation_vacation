8976ee1fde5cba54aec83109fc3e7bbfcce7d35a
anton <89879639755a@gmail.com>
2024-03-30 13:42:27 +0300

    Объявил bean для нескольких реализаций ICalculator

--- src/main/java/com/example/schedule/component/CalculatorBeanComponent.java
+++ src/main/java/com/example/schedule/component/CalculatorBeanComponent.java
@@ -1,2 +1,26 @@
-package com.example.schedule.component;public class CalculatorBeanComponent {
+package com.example.schedule.component;
+
+import com.example.schedule.model.calculator.date.CalculatorDayImpl;
+import com.example.schedule.model.calculator.day.CalculatorImpl;
+import com.example.schedule.model.calculator.ICalculator;
+import com.example.schedule.model.holiday.IHoliday;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class CalculatorBeanComponent {
+
+
+    private final IHoliday iHoliday;
+
+    @Bean
+    public ICalculator iCalculator() {
+        return new CalculatorImpl(iHoliday);
+    }
+    @Bean
+    public ICalculator iCalculatorDay() {
+        return new CalculatorDayImpl(iHoliday);
+    }
 }
